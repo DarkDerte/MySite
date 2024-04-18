ff5fde8f630443bf842c5c3a422995c48b16162e
DarkDerte <darkderte@gmail.com>
2024-04-18 23:33:57 +0200

    publish

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://darkderte.github.io',
+    base: 'MySite',
+});
